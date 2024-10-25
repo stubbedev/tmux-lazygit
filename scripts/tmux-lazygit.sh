388fc336eee9a3eb302d4ca11a200a50c7068a3b
Alexander Stage <abs@stubbe.dev>
2024-10-25 17:46:42 +0200

    added keybind htop

--- scripts/tmux-lazygit.sh
+++ scripts/tmux-lazygit.sh
@@ -8,6 +8,7 @@ source "$CURRENT_DIR/utils.sh"
 main() {
 	tmux set-environment -g TMUX_LAZYGIT_COMMANDS_FILE "$COMMANDS_FILE"
   tmux bind -n M-g run-shell "#{TMUX_LAZYGIT_COMMANDS_FILE} toggle_lazygit_window"
+  tmux bind -n M-a run-shell "#{TMUX_LAZYGIT_COMMANDS_FILE} toggle_htop_window"
 }
 
 main
