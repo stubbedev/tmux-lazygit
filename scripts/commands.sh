6347a01fd7b486959d589a95b2cd6580f1240cdd
Alexander Stage <abs@stubbe.dev>
2024-10-25 17:41:04 +0200

    added htop bind

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -30,3 +30,26 @@ toggle_lazygit_window() {
 if [[ "$1" == "toggle_lazygit_window" ]]; then
 	toggle_lazygit_window
 fi
+
+toggle_htop_window() {
+  local htop_window_name="htop"
+  local current_window=$(get_current_window)
+  local current_path=$(tmux display-message -p -F "#{pane_current_path}")
+
+  if [ "$current_window" = "$htop_window_name" ]; then
+    tmux last-window
+    return
+  fi
+
+  if [ "$(check_if_window_exists "$htop_window_name")" -eq 1 ]; then
+    tmux select-window -t "$htop_window_name"
+  else
+    tmux new-window -c "$current_path" -n "$htop_window_name"
+    tmux select-window -t "$htop_window_name"
+    tmux send-keys -t "$htop_window_name" "~/.stubbe/bin/htop_wrapper" C-m
+  fi
+}
+
+if [[ "$1" == "toggle_htop_window" ]]; then
+	toggle_htop_window
+fi
