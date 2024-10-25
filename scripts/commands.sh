de192e3fb05f66f2044549256bfd4a33bc37a668
Alexander Stage <abs@stubbe.dev>
2024-10-25 09:38:52 +0200

    simplify and change default binds

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -5,22 +5,27 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$CURRENT_DIR/utils.sh"
 
 toggle_lazygit_window() {
-	local lazygit_window_name="lazygit"
-	local current_window=$(get_current_window)
-	local current_path=$(tmux display-message -p -F "#{pane_current_path}")
 
-	if [ "$current_window" = "$lazygit_window_name" ]; then
-		tmux last-window
-		return
-	fi
+  if [ -d .git ] && echo .git || git rev-parse --git-dir > /dev/null 2>&1; then
+    local lazygit_window_name="lazygit"
+    local current_window=$(get_current_window)
+    local current_path=$(tmux display-message -p -F "#{pane_current_path}")
 
-	if [ "$(check_if_window_exists "$lazygit_window_name")" -eq 1 ]; then
-		tmux select-window -t "$lazygit_window_name"
-	else
-		tmux new-window -c "$current_path" -n "$lazygit_window_name"
-		tmux select-window -t "$lazygit_window_name"
-		tmux send-keys -t "$lazygit_window_name" "lazygit" C-m
-	fi
+    if [ "$current_window" = "$lazygit_window_name" ]; then
+      tmux last-window
+      return
+    fi
+
+    if [ "$(check_if_window_exists "$lazygit_window_name")" -eq 1 ]; then
+      tmux select-window -t "$lazygit_window_name"
+    else
+      tmux new-window -c "$current_path" -n "$lazygit_window_name"
+      tmux select-window -t "$lazygit_window_name"
+      tmux send-keys -t "$lazygit_window_name" "lazygit" C-m
+    fi
+  else
+    exit 0
+  fi
 }
 
 if [[ "$1" == "toggle_lazygit_window" ]]; then
